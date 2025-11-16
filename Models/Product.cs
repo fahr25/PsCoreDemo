c1d2ba1951dec0d5affdb86ca828ce9b6122f57f
fahr25 <fhernandez@icstars.org>
2025-11-15 19:24:27 -0600

    created Product Interface and Category Model.

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PsCoreDemo.Models;
+
+public abstract class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+
+    public int Points { get; set; }
+    public int Inventory { get; set; }
+
+    public int MinAge { get; set; }
+    public int MaxAge { get; set; }
+
+    // A list like ["Games", "Toys"] or ["Electronics", "Books"]
+    public List<Category> Categories { get; set; } = new();
+}
